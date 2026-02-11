f11f3c90b5bd4b8b41eaa9f6a1f75ce2f9ac54ec
haarshjanmi <harshjani88@gmail.com>
2026-02-12 02:18:59 +0530

    splved cors error

--- axios/api.ts
+++ axios/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000/api',
+  baseURL: '/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
